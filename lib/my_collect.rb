7a1803f8785c3a27380a6541187feb61b89286cd
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-10-23 09:08:58 -0400

    commit tree e13209e4830487d0689b5c10f273a37816286691

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+	counter = 0
+	new_array = []
+	while counter < array.length
+		new_value = yield(array[counter])
+		new_array.push(new_value)
+		counter += 1
+	end
+	new_array
+end
 
